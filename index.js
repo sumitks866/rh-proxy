448db4f7232bae01fdce535b8bc156d539bab4a7
Sumit Kumar Singh <sumitks866@gmail.com>
2022-10-13 16:45:22 +0530

    move server.js to /

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const config = require("../config");
-const app = require("./app");
+const config = require("./config");
+const app = require("./src/app");
 
 const port = config.PORT || 8001;
 const ip = config.IP || "127.0.0.1";
